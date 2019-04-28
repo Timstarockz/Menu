fa56dd8b79fb12c43b03cf549cd017395daec6fb
Tim :D <timothy5216@me.com>
2019-04-27 21:20:17 -0700

    cleaned up code in MenuView and added a notification for dismiss

--- MenuTest/Menu/MenuView.swift
+++ MenuTest/Menu/MenuView.swift
@@ -9,38 +9,28 @@
 import UIKit
 import SnapKit
 
-//MARK: - MenuView
+// MARK: - Variables, Enums & Initialization
 
 public class MenuView: UIView, MenuThemeable, UIGestureRecognizerDelegate {
     
-    public static let menuWillPresent = Notification.Name("CodeaMenuWillPresent")
+    // MARK: - Enums
     
-    private let titleLabel = UILabel()
-    private let gestureBarView = UIView()
-    private let tintView = UIView()
-    private let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
-    private let feedback = UISelectionFeedbackGenerator()
+    public enum Alignment {
+        case left
+        case center
+        case right
+    }
+    
+    // MARK: - Public
+    
+    public static let dismissMenu = Notification.Name("CodeaDissmissMenu")
+    public static let menuWillPresent = Notification.Name("CodeaMenuWillPresent")
     
     public var title: String {
         didSet {
             titleLabel.text = title
             contents?.title = title
         }
-    }
-    
-    private var menuPresentationObserver: Any!
-    
-    private var contents: MenuContents?
-    private var theme: MenuTheme
-    private var longPress: UILongPressGestureRecognizer!
-    private var tapGesture: UITapGestureRecognizer!
-    
-    private let itemsSource: () -> [MenuItem]
-    
-    public enum Alignment {
-        case left
-        case center
-        case right
     }
     
     public var contentAlignment = Alignment.right {
@@ -53,80 +43,100 @@ public class MenuView: UIView, MenuThemeable, UIGestureRecognizerDelegate {
         }
     }
     
+    // MARK: - Private
+
+    private let titleLabel = UILabel()
+    private let gestureBarView = UIView()
+    private let tintView = UIView()
+    private let effectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+    private let feedback = UISelectionFeedbackGenerator()
+    
+    private var menuPresentationObserver: Any!
+    
+    private var contents: MenuContents?
+    private var theme: MenuTheme
+    private var longPress: UILongPressGestureRecognizer!
+    private var tapGesture: UITapGestureRecognizer!
+    
+    private let itemsSource: () -> [MenuItem]
+    
+    private var gestureStart: Date = .distantPast
+    
+    private var isShowingContents: Bool {
+        return contents != nil
+    }
+    
+    // --
+    
     public init(title: String, theme: MenuTheme, itemsSource: @escaping () -> [MenuItem]) {
         self.itemsSource = itemsSource
         self.title = title
         self.theme = theme
-        
         super.init(frame: .zero)
         
+        // init title label
         titleLabel.text = title
         titleLabel.textColor = theme.darkTintColor
         titleLabel.textAlignment = .center
         titleLabel.setContentHuggingPriority(.required, for: .horizontal)
         
+        // init clipping view
         let clippingView = UIView()
         clippingView.clipsToBounds = true
-        
         addSubview(clippingView)
-        
-        clippingView.snp.makeConstraints {
-            make in
-            
-            make.edges.equalToSuperview()
-        }
-        
         clippingView.layer.cornerRadius = 8.0
-        
         clippingView.addSubview(effectView)
-        
-        effectView.snp.makeConstraints {
-            make in
-            
+        clippingView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
         
+        // init layout for effectView
+        effectView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
         effectView.contentView.addSubview(tintView)
         effectView.contentView.addSubview(titleLabel)
         effectView.contentView.addSubview(gestureBarView)
         
-        tintView.snp.makeConstraints {
-            make in
-            
+        // init layout for tint view
+        tintView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
         
-        titleLabel.snp.makeConstraints {
-            make in
-            
+        // init layout for title label
+        titleLabel.snp.makeConstraints { make in
             make.left.right.equalToSuperview().inset(12)
             make.centerY.equalToSuperview()
         }
         
+        // init gesture bar view
         gestureBarView.layer.cornerRadius = 1.0
-        gestureBarView.snp.makeConstraints {
-            make in
-            
+        gestureBarView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.height.equalTo(2)
             make.width.equalTo(20)
             make.bottom.equalToSuperview().inset(3)
         }
         
+        // init long press gesture recog
         longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPressGesture(_:)))
         longPress.minimumPressDuration = 0.0
         longPress.delegate = self
         addGestureRecognizer(longPress)
         
+        // init tap gesture recog
         tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
         tapGesture.delegate = self
         addGestureRecognizer(tapGesture)
         
+        // set given theme
         applyTheme(theme)
         
-        menuPresentationObserver = NotificationCenter.default.addObserver(forName: MenuView.menuWillPresent, object: nil, queue: nil) {
-            [weak self] notification in
-            
+        // add observer for menu will present notification
+        NotificationCenter.default.addObserver(forName: MenuView.dismissMenu, object: nil, queue: nil) { [weak self] (noti) in
+            self?.hideContents(animated: true)
+        }
+        menuPresentationObserver = NotificationCenter.default.addObserver(forName: MenuView.menuWillPresent, object: nil, queue: nil) { [weak self] notification in
             if let poster = notification.object as? MenuView, let this = self, poster !== this {
                 self?.hideContents(animated: false)
             }
@@ -134,22 +144,110 @@ public class MenuView: UIView, MenuThemeable, UIGestureRecognizerDelegate {
     }
     
     deinit {
+        NotificationCenter.default.removeObserver(self)
         NotificationCenter.default.removeObserver(menuPresentationObserver)
     }
-    
-    //MARK: - Required Init
     
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    //MARK: - Gesture Handling
+}
+
+// MARK: - Public Interface
+
+extension MenuView {
     
-    private var gestureStart: Date = .distantPast
+    public func showContents() {
+        NotificationCenter.default.post(name: MenuView.menuWillPresent, object: self)
+        
+        let contents = MenuContents(name: title, items: itemsSource(), theme: theme)
+        for view in contents.stackView.arrangedSubviews {
+            if let view = view as? MenuItemView {
+                var updatableView = view
+                updatableView.updateLayout = {
+                    [weak self] in
+                    
+                    self?.relayoutContents()
+                }
+            }
+        }
+        addSubview(contents)
+        contents.snp.makeConstraints { make in
+            switch contentAlignment {
+            case .left:
+                make.top.right.equalToSuperview()
+            case .right:
+                make.top.left.equalToSuperview()
+            case .center:
+                make.top.centerX.equalToSuperview()
+            }
+        }
+        
+        effectView.isHidden = true
+        
+        longPress?.minimumPressDuration = 0.07
+        
+        self.contents = contents
+        
+        setNeedsLayout()
+        layoutIfNeeded()
+        
+        contents.generateMaskAndShadow(alignment: contentAlignment)
+        contents.focusInitialViewIfNecessary()
+        
+        feedback.prepare()
+        contents.highlightChanged = {
+            [weak self] in
+            
+            self?.feedback.selectionChanged()
+        }
+    }
+    
+    public func hideContents(animated: Bool) {
+        let contentsView = contents
+        contents = nil
+        
+        longPress?.minimumPressDuration = 0.0
+        
+        effectView.isHidden = false
+        
+        if animated {
+            UIView.animate(withDuration: 0.2, animations: {
+                contentsView?.alpha = 0.0
+            }) {
+                finished in
+                contentsView?.removeFromSuperview()
+            }
+        } else {
+            contentsView?.removeFromSuperview()
+        }
+    }
+    
+}
+
+// MARK: - Private Interface
+
+extension MenuView {
+    
+    private func relayoutContents() {
+        if let contents = contents {
+            setNeedsLayout()
+            layoutIfNeeded()
+            
+            contents.generateMaskAndShadow(alignment: contentAlignment)
+        }
+    }
+    
+}
+
+// MARK: - Gesture Recognition
+
+extension MenuView {
     
     @objc private func longPressGesture(_ sender: UILongPressGestureRecognizer) {
         
-        //Highlight whatever we can
+        // Highlight whatever we can
         if let contents = self.contents {
             let localPoint = sender.location(in: self)
             let contentsPoint = convert(localPoint, to: contents)
@@ -158,7 +256,7 @@ public class MenuView: UIView, MenuThemeable, UIGestureRecognizerDelegate {
                 contents.highlightedPosition = CGPoint(x: contentsPoint.x, y: localPoint.y)
             }
         }
-
+        
         switch sender.state {
         case .began:
             if !isShowingContents {
@@ -218,93 +316,11 @@ public class MenuView: UIView, MenuThemeable, UIGestureRecognizerDelegate {
         return false
     }
     
-    public func showContents() {
-        NotificationCenter.default.post(name: MenuView.menuWillPresent, object: self)
-        
-        let contents = MenuContents(name: title, items: itemsSource(), theme: theme)
-        
-        for view in contents.stackView.arrangedSubviews {
-            if let view = view as? MenuItemView {
-                var updatableView = view
-                updatableView.updateLayout = {
-                    [weak self] in
-                    
-                    self?.relayoutContents()
-                }
-            }
-        }
-        
-        addSubview(contents)
-        
-        contents.snp.makeConstraints {
-            make in
-        
-            switch contentAlignment {
-            case .left:
-                make.top.right.equalToSuperview()
-            case .right:
-                make.top.left.equalToSuperview()
-            case .center:
-                make.top.centerX.equalToSuperview()
-            }
-        }
-        
-        effectView.isHidden = true
-        
-        longPress?.minimumPressDuration = 0.07
-        
-        self.contents = contents
-        
-        setNeedsLayout()
-        layoutIfNeeded()
-        
-        contents.generateMaskAndShadow(alignment: contentAlignment)
-        contents.focusInitialViewIfNecessary()
-        
-        feedback.prepare()
-        contents.highlightChanged = {
-            [weak self] in
-            
-            self?.feedback.selectionChanged()
-        }
-    }
-    
-    public func hideContents(animated: Bool) {
-        let contentsView = contents
-        contents = nil
-        
-        longPress?.minimumPressDuration = 0.0
-        
-        effectView.isHidden = false
-        
-        if animated {
-            UIView.animate(withDuration: 0.2, animations: {
-                contentsView?.alpha = 0.0
-            }) {
-                finished in
-                contentsView?.removeFromSuperview()
-            }
-        } else {
-            contentsView?.removeFromSuperview()
-        }
-    }
-    
-    private var isShowingContents: Bool {
-        return contents != nil
-    }
-    
-    //MARK: - Relayout
-    
-    private func relayoutContents() {
-        if let contents = contents {
-            setNeedsLayout()
-            layoutIfNeeded()
-            
-            contents.generateMaskAndShadow(alignment: contentAlignment)
-        }
-    }
-    
-    //MARK: - Hit Testing
+}
+
+// MARK: - Hit Testing
+
+extension MenuView {
     
     public override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
         guard let contents = contents else {
@@ -336,7 +352,11 @@ public class MenuView: UIView, MenuThemeable, UIGestureRecognizerDelegate {
         return super.hitTest(point, with: event)
     }
     
-    //MARK: - Theming
+}
+
+// MARK: - Theming
+
+extension MenuView {
     
     public func applyTheme(_ theme: MenuTheme) {
         self.theme = theme
@@ -353,4 +373,5 @@ public class MenuView: UIView, MenuThemeable, UIGestureRecognizerDelegate {
     public override func tintColorDidChange() {
         titleLabel.textColor = tintColor
     }
+    
 }
